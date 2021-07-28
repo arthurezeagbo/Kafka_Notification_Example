e554700f3e27afe6a9101ff6c82b1e373fa092bd
Arthur Ezeagbo <arthurezeagbo@gmail.com>
2021-07-28 11:17:52 +0100

    first commit

---
+++ src/test/java/com/arthurezeagbo/notification/producer/NotificationProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurezeagbo.notification.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
